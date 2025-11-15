18185dd0023288fe8cffad275416264eda170b2f
bvym <bvym1968@gmail.com>
2025-11-15 12:30:03 -0500

    Fix JAR manifest for deployment

--- build.gradle.kts
+++ build.gradle.kts
@@ -41,6 +41,15 @@ application {
     mainClass.set("MainKt")
 }
 
+tasks.jar {
+    manifest {
+        attributes["Main-Class"] = "MainKt"
+    }
+    // Include dependencies in JAR
+    duplicatesStrategy = DuplicatesStrategy.EXCLUDE
+    from(configurations.runtimeClasspath.get().map { if (it.isDirectory) it else zipTree(it) })
+}
+
 kotlin {
     jvmToolchain(17)
 }
